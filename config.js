ed92d66cf27c969fa08aa5ddd26a4e24752b1827
xSadu <150851832+xSadu@users.noreply.github.com>
2024-10-30 20:30:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '' process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://imgur.com/a/ydovE9P",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/xSadu/ZER-BAIRANGA-MD/blob/main/img/611B4CFF-426D-4DDB-9141-9F6FCF250B36.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello... 👋 Im Zer Bairanga 😌🤝 Im alive now...!",
 };
